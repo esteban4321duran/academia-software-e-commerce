e4c8c4260e451aeca6fba257757033bd0fef06f6
Esteban <esteban321duran@gmail.com>
2022-08-24 17:43:06 -0300

    web client initial config

---
+++ cliente-web/src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+    return (
+        <div className="bg-slate-900 min-h-screen grid grid-cols-3 grid-rows-2 justify-items-center items-center">
+            <h1 className="text-5xl text-sky-500 col-span-3 font-bold font-sans">
+                Hello React!
+            </h1>
+        </div>
+    );
+}
+
+export default App;
